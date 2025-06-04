810ed8eb61d2fb5631d62e1b75972353469450ee
Wasi Ahmad <wasiahmad0569@gmail.com>
2025-06-05 05:50:07 +0600

    Heroku

--- app.py
+++ app.py
@@ -149,4 +149,5 @@ def unblur_image():
         return jsonify({"error": "Incorrect password!"}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+
